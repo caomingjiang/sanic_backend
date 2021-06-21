faaf4fd07292c6cf473257c08fab33f2cabff0de
caomingjiang <709166693@qq.com>
2021-06-21 23:41:51 +0800

    开发部分color map功能

---
+++ apps/color_map/control.py
@@ -0,0 +1,8 @@
+from db import ColorMapDstiff, ColorMapNtfDr, ColorMapNtfRr, ColorMapSpindleNtfDr, ColorMapSpindleNtfRr
+
+
+class ColorMapData(object):
+    def __init__(self, se, car_info):
+        self.se = se
+        self.car_info = car_info
+
--- apps/color_map/view.py
+++ apps/color_map/view.py
@@ -1,31 +1,19 @@
-# from flask import Blueprint, request, Response
-# from common.common import JsonResponse, login_required, view_exception
-# from apps.car_body.control import ExportCarBodyData
-# from db import CarInfo, CarBody
-# from common import data_validate
-# from datetime import datetime
-#
-# bp = Blueprint('car_body', __name__, url_prefix='/api/v1/car_body/')
-#
-#
-# @bp.route('update_data', methods=['POST'])
-# @login_required
-# @view_exception(fail_msg='save_car_body_data failed', db_session=True)
-# def save_car_body_data(se):
-#     req_data = data_validate.SaveCarBodyData(**request.json)
-#     car_info = se.query(CarInfo).filter(CarInfo.is_dev == 1).first()
-#     if not car_info:
-#         return JsonResponse.fail('请先设置当前车型')
-#
-#     now = datetime.now()
-#     insert_list = []
-#     for data_type, cb_obj in req_data:
-#         insert_list.append(CarBody(
-#             data_type=data_type, value=cb_obj.value, score=cb_obj.score,
-#             update_time=now, create_time=now, car_info=car_info
-#         ))
-#     se.query(CarBody).filter(CarBody.car_info == car_info).delete()
-#     se.add_all(insert_list)
-#     se.commit()
-#     return JsonResponse.success()
-#
+from flask import Blueprint, request, Response
+from common.common import JsonResponse, login_required, view_exception
+from db import CarInfo
+from common import data_validate
+from datetime import datetime
+
+bp = Blueprint('color_map', __name__, url_prefix='/api/v1/color_map/')
+
+
+@bp.route('get_data', methods=['POST'])
+@login_required
+@view_exception(fail_msg='get_color_map_data failed', db_session=True)
+def get_color_map_data(se):
+    car_info = se.query(CarInfo).filter(CarInfo.is_dev == 1).first()
+    if not car_info:
+        return JsonResponse.fail('请先设置当前车型')
+
+    return JsonResponse.success()
+
