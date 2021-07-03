7f52d49f90a1ae822eb17ddca8d15cf3b96b38f4
liangdongkai <liang.dongkai@163.com>
2021-07-03 22:41:28 +0800

    add real_colourmap func and test func

--- ai/noise_algo_func.py
+++ ai/noise_algo_func.py
@@ -137,8 +137,28 @@ def ntf_colourmap(ori_df, type_name):
             score_ = rms_df[column].apply(lambda x: spindle_ntf_score(x))
         score_df[column] = score_.tolist()
         
-    return score_df 
-    
+    return score_df
+
+######real_colourmap
+def realcapture_colourmap(ori_df):
+    '''
+    ori_df = pd.read_excel('./最新输入模板，以此为准/原始输入_实测曲线.xlsx')
+    '''
+    ori_df.drop(ori_df.index[-1], inplace=True)
+    frequency = ori_df['频率'].apply(lambda x: int(x / 10))
+    ori_df = ori_df.drop('频率', axis=1).apply(np.square)
+    ori_df['频率'] = frequency
+
+    rms_df = ori_df.groupby("频率").sum().apply(lambda x: x / 10).apply(np.sqrt)
+    frequency = rms_df.reset_index()['频率'].apply(lambda x: str(x * 10) + '-' + str((x + 1) * 10))
+
+    score_df = pd.DataFrame()
+    score_df['频率'] = frequency
+    for column in rms_df.columns:
+        score_df[column] = rms_df[column].tolist()  ####主意tolist的坑
+
+    return score_df
+
 def Multi_Score_Predict(colourmap_dict, weights_dict, adjust_value=115):
     '''
     colourmap_dict : dstff_df, ntf_df, spindle_ntf_df, 
@@ -178,44 +198,110 @@ def Multi_Score_Predict(colourmap_dict, weights_dict, adjust_value=115):
         
     return DR_score, RR_score
 
-
-if __name__ == "__main__":
-    
+def test():
     ori_df = pd.read_excel('./最新输入模板，以此为准/原始输入_Dstiff.xlsx')
-    dstiff_colourmap = dstiff_colourmap(ori_df)
-    
+    dstiff_colourmap = ntf_colourmap(ori_df)
+
     ori_df = pd.read_excel('./最新输入模板，以此为准/原始输入_NTF-DR.xlsx')
     ntf_dr_colourmap = ntf_colourmap(ori_df, 'ntf')
-    
+
     ori_df = pd.read_excel('./最新输入模板，以此为准/原始输入_NTF-RR.xlsx')
     ntf_rr_colourmap = ntf_colourmap(ori_df, 'ntf')
-    
+
     ori_df = pd.read_excel('./最新输入模板，以此为准/原始输入_SpindleNTF-DR.xlsx')
     spindle_dr_colourmap = ntf_colourmap(ori_df, 'spindle_ntf')
-    
+
     ori_df = pd.read_excel('./最新输入模板，以此为准/原始输入_SpindleNTF-RR.xlsx')
     spindle_rr_colourmap = ntf_colourmap(ori_df, 'spindle_ntf')
-    
+
     colourmap_dict = {}
     colourmap_dict['dstiff'] = dstiff_colourmap
     colourmap_dict['ntf_dr'] = ntf_dr_colourmap
     colourmap_dict['ntf_rr'] = ntf_rr_colourmap
     colourmap_dict['spindle_dr'] = spindle_dr_colourmap
     colourmap_dict['spindle_rr'] = spindle_rr_colourmap
-    
+
     weights_dstiff = pd.read_excel('./权系数/专家权重-DSTIFF.xlsx')
     weights_ntf_dr = pd.read_excel('./权系数/专家权重-NTF-DR.xlsx')
     weights_ntf_rr = pd.read_excel('./权系数/专家权重-NTF-RR.xlsx')
     weights_spindle_dr = pd.read_excel('./权系数/专家权重-Spindle-NTF-DR.xlsx')
     weights_spindle_rr = pd.read_excel('./权系数/专家权重-Spindle-NTF-RR.xlsx')
-    
+
     weights_dict = {}
     weights_dict['dstiff'] = weights_dstiff
     weights_dict['ntf_dr'] = weights_ntf_dr
     weights_dict['ntf_rr'] = weights_ntf_rr
     weights_dict['spindle_dr'] = weights_spindle_dr
     weights_dict['spindle_rr'] = weights_spindle_rr
-    
-    
+
     DR_score, RR_score = Multi_Score_Predict(colourmap_dict, weights_dict)
+
+if __name__ == "__main__":
+    
+    ori_df = pd.read_excel('./最新输入模板，以此为准/原始输入_实测曲线.xlsx')
+    real_colourmap = realcapture_colourmap(ori_df)
+    
+    fuchejia = {
+	"轮胎_胎面辐射声（轮胎选型）":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"轮胎_力传递峰值频率":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"轮胎_力传递峰值":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"轮胎_操稳性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"轮胎_耐久性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"轮辋刚度_16-17’":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"轮辋刚度_18-19’":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前副车架（自由-自由）_全副车架(bend)模态":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前副车架（自由-自由）_半副车架(bend)模态":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架（自由-自由）_类型一：扭转梁(bend)模态":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架（自由-自由）_类型二：多连杆(横梁弯曲)模态":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]]
+}
+
+    a = {
+	"前下摆臂handling衬套_X向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前下摆臂handling衬套_Y向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前下摆臂handling衬套_Z向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前下摆臂handling衬套_操稳性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前下摆臂handling衬套_耐久性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前下摆臂ride衬套_X向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前下摆臂ride衬套_Y向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前下摆臂ride衬套_Z向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前下摆臂ride衬套_操稳性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"前下摆臂ride衬套_耐久性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架前衬套_X向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架前衬套_Y向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架前衬套_Z向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架前衬套_操稳性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架前衬套_耐久性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架后衬套_X向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架后衬套_Y向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架后衬套_Z向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架后衬套_操稳性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后副车架后衬套_耐久性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"刀锋臂衬套_X向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"刀锋臂衬套_Y向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"刀锋臂衬套_Z向静刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"刀锋臂衬套_操稳性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"刀锋臂衬套_耐久性能":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]]
+}
+
+
+    b = {
+	"白车身_全局扭转模态频率":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"白车身_全局弯曲模态频率":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"C ring_Visual Check":{"A":10,"B":8,"C":6,"D":4},
+	"前围_大板刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"一号梁_Visual Check":{"A":10,"B":8,"C":6,"D":4},
+	"地板_大板刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"地板无支撑板面积_Visual Check":{"A":10,"B":8,"C":6,"D":4},
+	"备胎舱地板_大板刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"备胎舱加强板设计_Visual Check":{"A":10,"B":8,"C":6,"D":4},
+	"顶棚_大板刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"顶棚结构_Visual Check":{"A":10,"B":8,"C":6,"D":4},
+	"左轮罩内板_大板刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后减震器加强板_Visual Check":{"A":10,"B":8,"C":6,"D":4},
+	"右轮罩内板_大板刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"后侧围外板_大板刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"衣帽架_大板刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"落水槽_大板刚度":[[0,10,20,35,50,60], [0, 1,2,4,6,8,10]],
+	"落水槽支架设计_Visual Check":{"A":10,"B":8,"C":6,"D":4}
+}
 
